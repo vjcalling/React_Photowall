e93c1ea67dbe41fe04b48e65a26fd9f00ae4462b
vjcalling <vibhor.jain@outlook.com>
2018-04-05 21:27:49 +0530

    adding entries to posts

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -22,6 +22,10 @@ class Main extends Component {
                 id: "2",
                 description: "On a vacation!",
                 imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+                }, {
+                    id: "3",
+                    description: "Again On a vacation!",
+                    imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
                 }]
         }
     }
