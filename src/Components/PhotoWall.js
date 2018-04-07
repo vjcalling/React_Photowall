f6125e40e23337ac675a04017f6264abaa640cf6
vjcalling <vibhor.jain@outlook.com>
2018-04-07 17:02:54 +0530

    adding logic to show recent photo first

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -10,7 +10,11 @@ function PhotoWall(props){
                 {/* <a className="addIcon" onClick={props.onNavigate} href="#AddPhoto"></a> */}
                 <Link className="addIcon" to="/AddPhoto"></Link>
                 <div className="photoGrid"> 
-                        {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+                        {props.posts
+                            .sort(function(x,y) {
+                                return y.id - x.id
+                            })
+                            .map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
                 </div>
             </div>
 }
